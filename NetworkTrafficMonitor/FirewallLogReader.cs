25d8ded7fe2b24ead4196c5c9c129815b96d7ebb
broertje-cloud <65792288+broertje-cloud@users.noreply.github.com>
2025-04-09 05:57:11 +0200

    firewall toegevoegd

---
+++ NetworkTrafficMonitor/FirewallLogReader.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkTrafficMonitor
+{
+    class FirewallLogReader
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public async Task ScanFirewallLogAsync(string logFilePath)
+        {
+            if (!File.Exists(logFilePath))
+            {
+                Console.WriteLine("Firewall log niet gevonden.");
+                return;
+            }
+
+            //var lines = File.ReadAllLines(logFilePath);
+            ///FileShare.ReadWrite zorgt ervoor dat je het bestand mag lezen zelfs als een ander proces (zoals Windows Firewall) het aan het gebruiken is.
+            string[] lines;
+            using (var stream = new FileStream(logFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var reader = new StreamReader(stream))
+            {
+                var content = await reader.ReadToEndAsync();
+                lines = content.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            var ipList = new HashSet<string>();
+
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("20") && line.Contains("ALLOW") && line.Contains("OUT"))
+                {
+                    var parts = line.Split(' ');
+                    var destinationIP = parts.LastOrDefault();
+
+                    if (IPAddress.TryParse(destinationIP, out _) && !ipList.Contains(destinationIP))
+                    {
+                        ipList.Add(destinationIP);
+                        var location = await GetGeoLocation(destinationIP);
+                        Console.WriteLine($"{destinationIP} - {location}");
+                    }
+                }
+            }
+        }
+
+        private async Task<string> GetGeoLocation(string ip)
+        {
+            try
+            {
+                string apiUrl = $"http://ip-api.com/json/{ip}";
+                var response = await httpClient.GetAsync(apiUrl);
+                var json = JObject.Parse(await response.Content.ReadAsStringAsync());
+                return $"{json["country"]}, {json["city"]}";
+            }
+            catch
+            {
+                return "Locatie niet beschikbaar";
+            }
+        }
+    }
+
+}
--- NetworkTrafficMonitor/Program.cs
+++ NetworkTrafficMonitor/Program.cs
@@ -12,12 +12,17 @@ namespace NetworkTrafficMonitor
         static async Task Main()
         {
             Console.WriteLine("Netwerkverkeer monitor gestart...");
-            string logFilePath = "network_log.txt"; // Pad naar het logbestand
+            string logFilePath = @"C:\Tools\NetwerkMonitorApp\network_log.txt";
 
             NetworkLogger logger = new NetworkLogger(logFilePath, new SuspiciousIPChecker());
             await logger.LogActiveConnectionsWithGeoInfo();
 
             Console.WriteLine("Logging opgeslagen in: " + logFilePath); // Bevestiging dat de log is opgeslagen
+
+            string firewallLogPath = @"C:\Windows\System32\LogFiles\Firewall\pfirewall.log";
+            FirewallLogReader reader = new FirewallLogReader();
+            await reader.ScanFirewallLogAsync(firewallLogPath);
+
         }
     }
 
